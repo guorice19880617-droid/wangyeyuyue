ee80232b4bd4aa26763aee88e7d74d4d1dbac2cd
guorice19880617-droid <guorice19880617@gmail.com>
2026-03-12 07:11:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# 预约数据
+days = ["周一", "周二", "周三"]
+times = ["10:00", "11:00", "12:00"]
+
+booking = {}
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        days=days,
+        times=times,
+        booking=booking
+    )
+
+@app.route("/book", methods=["POST"])
+def book():
+
+    day = request.form["day"]
+    time = request.form["time"]
+
+    key = f"{day}_{time}"
+
+    if key in booking:
+        return "该时间已被预约"
+
+    booking[key] = "已预约"
+
+    return "预约成功 <br><a href=' '>返回</a >"
+
+
+if __name__ == "__main__":
+    app.run()
