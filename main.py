5570f8fd70fdba01e33167f66ca30ef05f9ca41c
guorice19880617-droid <guorice19880617@gmail.com>
2026-03-12 07:35:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-# 预约数据
-days = ["周一", "周二", "周三"]
-times = ["10:00", "11:00", "12:00"]
+# 自动生成未来7天日期
+days = []
+for i in range(7):
+    day = datetime.now() + timedelta(days=i)
+    days.append(day.strftime("%m-%d"))
 
+# 每天时间段
+times = ["10:00", "11:00", "12:00", "13:00"]
+
+# 预约记录
 booking = {}
 
 @app.route("/")
 def home():
+    return render_template("index.html", days=days)
+
+@app.route("/day/<day>")
+def day_page(day):
+
     return render_template(
-        "index.html",
-        days=days,
+        "times.html",
+        day=day,
         times=times,
         booking=booking
     )
@@ -20,18 +32,22 @@ def home():
 @app.route("/book", methods=["POST"])
 def book():
 
+    name = request.form["name"]
     day = request.form["day"]
     time = request.form["time"]
 
     key = f"{day}_{time}"
 
     if key in booking:
-        return "该时间已被预约"
+        return "该时间已被预约 <br><a href=' '>返回首页</a >"
 
-    booking[key] = "已预约"
+    booking[key] = name
 
-    return "预约成功 <br><a href=' '>返回</a >"
+    return "预约成功 <br><a href='/'>返回首页</a >"
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", booking=booking)
 
 if __name__ == "__main__":
     app.run()
